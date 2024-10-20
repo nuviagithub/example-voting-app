a88409bdf940060c2a093e950ae8ac701cf9c8e5
nuviagithub <127896414+nuviagithub@users.noreply.github.com>
2024-10-20 07:24:56 -0700

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "NDC - John Mahama")
-option_b = os.getenv('OPTION_B', "NPP - Mahamadu Bawumia")
+option_b = os.getenv('OPTION_B', "NPP - Mahamudu Bawumia")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
